fdc53cbeb36175d730c32896b13b422844095d23
Denis Sabani <denissabani@gmx.de>
2026-08-19 22:35:55 +0200

    Initial Commit, Tracker Class added

---
+++ main.py
@@ -0,0 +1,9 @@
+from tracker import *
+
+def main():
+    t = Tracker()
+    print(t.build_tracking_link("1Y5888Z10444051908"))
+    
+
+if __name__ == "__main__":
+    main()
---
+++ tracker.py
@@ -0,0 +1,35 @@
+class Tracker:
+    CARRIER_URLS = {
+        "UPS": "https://www.ups.com/track?tracknum={}&loc=de_DE",
+        "DHL": "https://www.dhl.de/de/privatkunden/pakete-empfangen/verfolgen.html?piececode={}",
+    }
+
+    _KEYBOARD_SWAP = str.maketrans({"Z":"Y", "Y":"Z"})
+
+    def fix_UPS_keyboard_layout_error(self, tn: str) -> str:
+        clean_tn = tn.strip().upper()
+        if clean_tn.startswith("1Y"):
+            return self.swap(clean_tn)
+        return clean_tn
+
+    def swap(self, tn: str) -> str:
+        return tn.translate(self._KEYBOARD_SWAP)
+    
+    def detect_carrier(self, tn: str) -> str | None:
+        if tn.startswith("1Z"):
+            return "UPS"
+        if tn.startswith(("JD", "JJ", "0034")):
+            return "DHL"
+        else:
+            return "No carrier could have been detected."
+
+    def build_tracking_link(self, tn: str, used_carrier: str = None) -> str:
+        clean_tn = self.fix_UPS_keyboard_layout_error(tn)
+
+        if not clean_tn:
+            return ""
+
+        carrier = used_carrier.upper() if used_carrier else self.detect_carrier(clean_tn)
+        template = self.CARRIER_URLS.get(carrier)
+
+        return template.format(clean_tn) if template else ""
